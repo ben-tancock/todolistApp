f0e01ffed210160e72ec35899bbb4f22094246ba
ben-tancock <ben.tancock@gmail.com>
2020-09-09 14:41:47 -0700

    log

--- src/server.js
+++ src/server.js
@@ -302,4 +302,6 @@ app.use(express.static(__dirname + '/dist/todolist'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
     '/dist/angular-on-heroku/login.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log("express server listening on some port");
+});
