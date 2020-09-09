9803f1e0ceb6e827ffd1350c19ef6fe33adf36cd
ben-tancock <ben.tancock@gmail.com>
2020-09-09 16:12:44 -0700

    can't use relative paths

--- src/server.js
+++ src/server.js
@@ -301,7 +301,7 @@ const app = express();
 app.use(express.static(__dirname + '/dist/todolist'));
 app.get('/*', function(req,res) {
   console.log("server get request");
-  res.sendFile('index.html');});
+  res.sendFile(__dirname + 'index.html');});
 app.listen(process.env.PORT || 8080, () => {
   console.log("express server listening on some port");
 });
