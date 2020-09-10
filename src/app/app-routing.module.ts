fa96645cb7c4de667b3307c723d3f7e51b7a33ec
ben-tancock <ben.tancock@gmail.com>
2020-09-09 21:47:28 -0700

    made app component route to login upon initialization

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+/*import { TodoComponent } from './todo/todo.component';
+import { NewNavComponent } from './new-nav/new-nav.component';
+import { TaskGuardGuard } from './task-guard.guard';*/
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+  /*{
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },*/
+
+  /*{
+    path: 'tasks',
+    component: NewNavComponent,
+    canActivate: [TaskGuardGuard]
+  },*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'to-do-heroku';
+export class AppComponent implements OnInit {
+  title = "to-do-heroku";
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['/login']);
+
+  }
 }
