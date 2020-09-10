303c6d26eb91b2f3a438f651cfdd788dc429d239
ben-tancock <ben.tancock@gmail.com>
2020-09-09 19:52:05 -0700

    server.js typo

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/angular-on-heroku'));
+app.use(express.static(__dirname + '/dist/to-do-heroku'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/angular-on-heroku/index.html'));});
+    '/dist/to-do-heroku/index.html'));});
 app.listen(process.env.PORT || 8080, function(){
   console.log("server listening on some port");
 });
