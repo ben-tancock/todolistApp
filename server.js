8a4e4106978259260e4a478f8369ea0732609b6f
ben-tancock <ben.tancock@gmail.com>
2020-10-31 14:59:51 -0700

    changed requiredHeader

--- server.js
+++ server.js
@@ -383,7 +383,7 @@ else{
 
   cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
-    requireHeader: ['origin', 'x-requested-with'],
+    requireHeader: ['origin'],
     removeHeaders: ['cookie', 'cookie2']
   }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
