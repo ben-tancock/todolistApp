d2789167233b13797a7d6dbd47efc63815a6440b
ben-tancock <ben.tancock@gmail.com>
2020-11-03 16:31:47 -0800

    removing cors-anywhere

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://to-do-bentancock.herokuapp.com'
+  apiUrl: 'https://to-do-bentancock.herokuapp.com'
 };
